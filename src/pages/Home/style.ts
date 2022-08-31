cecd9eda6c6ead0a82285a34284bfcc90fb29343
Myke Vida de Macedo <myke.programacao@gmail.com>
2022-08-30 22:48:44 -0300

    feat: criação do componente de Header

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -2,7 +2,12 @@ import styled, { css } from "styled-components"
 
 export const HomeStyled = styled.div`
     width: 100%;
-    height: 100vh;
+    height: 100%;
+    min-height: 100vh;
 
     background-color: #222;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
 `
