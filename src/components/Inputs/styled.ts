aaaf732767326f8e63fbb883ccd10e9a0fff2190
unknown <brenos93@gmail.com>
2022-08-31 14:35:01 -0300

    feat: corrindo cor do input

--- src/components/Inputs/styled.ts
+++ src/components/Inputs/styled.ts
@@ -2,10 +2,23 @@ import { FormControl } from "@mui/material";
 import styled from "styled-components";
 
 export const FormControlEdited = styled(FormControl)`
-  width: 320px;
+  width: 100%;
+  max-width: 320px;
   height: 48px;
 
-  @media (max-width: 425px) {
-    width: 280px;
+  .MuiButtonBase-root {
+    color: #d8d8d8;
+  }
+
+  .MuiInputBase-input {
+    color: #f5f5f5;
+  }
+
+  .MuiFormLabel-root {
+    color: #d8d8d8;
+  }
+
+  .MuiInput-root::before {
+    border-bottom: 1px solid #d8d8d8;
   }
 `;
