80c6dbbe821fa13d0c5203a9dadd29e46ff700d4
Victor Cruz <motoggb979@gmail.com>
2022-08-31 12:43:13 -0300

    feat: estilização inicial com posicionamento das divs desktop

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Global from "./styles/global";
 import Routes from "./routes";
 import { Toaster } from "react-hot-toast";
 import "./styles/App.css";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
+      <Footer />
       <Global />
       <Routes />
       <Toaster />
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -32,6 +32,7 @@ const Footer = () => {
             </li>
           </ul>
         </div>
+        <div className="lineFooter"></div>
         <div className="containerInfos">
           <div className="terms">
             <MdGavel />
