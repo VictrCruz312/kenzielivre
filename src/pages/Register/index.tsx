f9d563652eb70ff6f8426fc9bab884905998e55e
Victor Cruz <motoggb979@gmail.com>
2022-09-01 13:42:55 -0300

    feat: estilização e responsividade da página de registro, hook form para formulário funcional só falta o Yup para validações

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,83 +1,106 @@
-import Box from "../../components/Box"
-import TransitionPage from "../../components/TransitionPage"
-import { Block, RegisterStyled } from "./style"
-import Form from "./components/Form"
+import Box from "../../components/Box";
+import TransitionPage from "../../components/TransitionPage";
+import { Block, FormStyled, RegisterStyled } from "./style";
 
-import Select from "../../components/Select"
-import { useForm } from "react-hook-form"
-import Apresentation from "../../components/modalApresentacao"
-import Input from "../../components/Inputs"
-import Checkbox from "./components/Checkbox"
-import { ButtonAll } from "../../components/Button"
-import TextArea from "../../components/TextArea"
-import { Button } from "@mui/material"
-const Register = () => {
+import Select from "../../components/Select";
+import { useForm } from "react-hook-form";
+import Apresentation from "../../components/modalApresentacao";
+import Input from "../../components/Inputs";
+import Checkbox from "./components/Checkbox";
+import { ButtonAll } from "../../components/Button";
 
-    const { register, handleSubmit } = useForm()
-
-    const onSubmit = ( data:any ) => {
-        console.log( data )
-    }
-
-    return(
-        <TransitionPage>
-            <RegisterStyled>
-                    <h1 className="block__logo">Kenzie Livre</h1>
-                <Block>
-                    <Apresentation
-                        title="Não tem uma conta?"
-                        description="Cadastre-se já e encontre os melhores produtos pelo menor preço."
-                    />
-                </Block>
-                <Box
-                    width="small"
-                    minWidth="563px"
-                    widthMobile="100%"
-                    height="100%"
-                >   
-                    <Form onSubmit={handleSubmit(onSubmit)} name="Criar Conta">
-                        <Input
-                             placeholder="Nome"
-                             type="text"
-                             isText
-                        />
-                        <Input
-                             placeholder="Sobrenome"
-                             type="text"
-                             isText
-                        />
-                        <Input
-                             placeholder="Email"
-                             type="text"
-                             isText
-                        />
-                        <Input
-                             placeholder="Senha"
-                             type="password"
-                        />
-                        <Input
-                             placeholder="Confirmar Senha"
-                             type="password"
-                        />
-                        <Select
-                            arrayText={["Vendedor", "Cliente"]}
-                            register={register}
-                            name="teste"
-                            label="Tipo de conta"
-                        />
-                        <Checkbox/>
-                        <ButtonAll background="deft" size="small">
-                            Criar conta
-                        </ButtonAll>
-                        <p className="form__optionsText">Já tem uma conta?</p>
-                        <ButtonAll background="transp" size="small">
-                            Entrar
-                        </ButtonAll>
-                        <button type="submit">teste</button>
-                    </Form>
-                </Box>
-            </RegisterStyled>
-        </TransitionPage>
-    )
+export interface IDataRegister {
+  nome: string;
+  sobrenome: string;
+  email: string;
+  senha: string;
+  confirmarSenha: string;
+  type: string;
 }
-export default Register
+
+const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IDataRegister>();
+
+  const onSubmit = (data: IDataRegister) => {
+    console.log(data);
+  };
+
+  return (
+    <TransitionPage>
+      <RegisterStyled>
+        <div className="MainRegister">
+          <h1 className="block__logo">Kenzie Livre</h1>
+          <Block>
+            <Apresentation
+              title="Não tem uma conta?"
+              description="Cadastre-se já e encontre os melhores produtos pelo menor preço."
+            />
+          </Block>
+          <Box width="small" minWidth="563px" widthMobile="100%" height="100%">
+            <FormStyled onSubmit={handleSubmit(onSubmit)}>
+              <h2 className="form__title">Criar Conta</h2>
+
+              <Input
+                placeholder="Nome"
+                type="text"
+                isText
+                name="nome"
+                register={register}
+                message={errors?.nome}
+              />
+              <Input
+                placeholder="Sobrenome"
+                type="text"
+                isText
+                name="sobrenome"
+                register={register}
+                message={errors.sobrenome}
+              />
+              <Input
+                placeholder="Email"
+                type="text"
+                isText
+                name="email"
+                register={register}
+                message={errors.email}
+              />
+              <Input
+                placeholder="Senha"
+                type="password"
+                name="senha"
+                register={register}
+                message={errors.senha}
+              />
+              <Input
+                placeholder="Confirmar Senha"
+                type="password"
+                name="confirmarSenha"
+                register={register}
+                message={errors.confirmarSenha}
+              />
+              <Select
+                arrayText={["Vendedor", "Cliente"]}
+                register={register}
+                name="type"
+                label="Tipo de conta"
+              />
+              <Checkbox />
+              <ButtonAll background="deft" size="large">
+                Criar conta
+              </ButtonAll>
+              <p className="form__optionsText">Já tem uma conta?</p>
+              <ButtonAll background="transp" size="large">
+                Entrar
+              </ButtonAll>
+            </FormStyled>
+          </Box>
+        </div>
+      </RegisterStyled>
+    </TransitionPage>
+  );
+};
+export default Register;
