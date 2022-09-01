15170d7711e9d418440242913c8c501999669f12
Myke Vida de Macedo <myke.programacao@gmail.com>
2022-08-31 21:43:13 -0300

    feat: organização padrao de pagina em todos ela colocado

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,5 +1,16 @@
 import styled, { css } from "styled-components"
 
 export const LoginStyled = styled.div`
+    box-sizing: border-box;
 
+    width: 100%;
+    height: 100%;
+    min-height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
+    background-color: #1C1B23;
 `
