da7e05938612b887d69fded6599bc3a3eada7df5
unknown <brenos93@gmail.com>
2022-09-01 11:40:07 -0300

    feat: corrindo input, add register e menssagem de error

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,33 +1,29 @@
-import { InputLabel, MenuItem } from "@mui/material"
-import Select from '@mui/material/Select';
-import { SelectStyled } from "./style"
+import { InputLabel, MenuItem } from "@mui/material";
+import Select from "@mui/material/Select";
+import { SelectStyled } from "./style";
 
-import { v4 as uuid } from "uuid"
+import { v4 as uuid } from "uuid";
 
 interface IPropsSelect {
-    arrayText:string[],
-    register:any,
-    name:string,
-    label:string,
+  arrayText: string[];
+  register: any;
+  name: string;
+  label: string;
 }
 
-const SelectEdited = ( { arrayText, register, name, label }:IPropsSelect ) => {
+const SelectEdited = ({ arrayText, register, name, label }: IPropsSelect) => {
+  return (
+    <SelectStyled margin="dense" variant="standard">
+      <InputLabel id="Select">{label}</InputLabel>
+      <Select labelId="Select" id="Select" label={label} {...register(name)}>
+        {arrayText.map((text) => (
+          <MenuItem key={uuid()} value={text}>
+            {text}
+          </MenuItem>
+        ))}
+      </Select>
+    </SelectStyled>
+  );
+};
 
-    return(
-        <SelectStyled margin="dense" variant="standard" >
-            <InputLabel id="Select">{label}</InputLabel>
-            <Select
-                labelId="Select"
-                id="Select"
-                label={label}
-                {...register(name)}
-            >
-                {
-                    arrayText.map( text =>  <MenuItem key={uuid()} value={text}>{ text }</MenuItem> )
-                }
-            </Select>
-        </SelectStyled>
-    )
-}
-
-export default SelectEdited
+export default SelectEdited;
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,83 +1,88 @@
-import Box from "../../components/Box"
-import TransitionPage from "../../components/TransitionPage"
-import { Block, RegisterStyled } from "./style"
-import Form from "./components/Form"
+import Box from "../../components/Box";
+import TransitionPage from "../../components/TransitionPage";
+import { Block, RegisterStyled } from "./style";
+import Form from "./components/Form";
 
-import Select from "../../components/Select"
-import { useForm } from "react-hook-form"
-import Apresentation from "../../components/modalApresentacao"
-import Input from "../../components/Inputs"
-import Checkbox from "./components/Checkbox"
-import { ButtonAll } from "../../components/Button"
-import TextArea from "../../components/TextArea"
-import { Button } from "@mui/material"
+import Select from "../../components/Select";
+import { useForm } from "react-hook-form";
+import Apresentation from "../../components/modalApresentacao";
+import Input from "../../components/Inputs";
+import Checkbox from "./components/Checkbox";
+import { ButtonAll } from "../../components/Button";
+import TextArea from "../../components/TextArea";
+import { Button } from "@mui/material";
+
 const Register = () => {
+  const { register, handleSubmit } = useForm();
 
-    const { register, handleSubmit } = useForm()
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
 
-    const onSubmit = ( data:any ) => {
-        console.log( data )
-    }
-
-    return(
-        <TransitionPage>
-            <RegisterStyled>
-                    <h1 className="block__logo">Kenzie Livre</h1>
-                <Block>
-                    <Apresentation
-                        title="Não tem uma conta?"
-                        description="Cadastre-se já e encontre os melhores produtos pelo menor preço."
-                    />
-                </Block>
-                <Box
-                    width="small"
-                    minWidth="563px"
-                    widthMobile="100%"
-                    height="100%"
-                >   
-                    <Form onSubmit={handleSubmit(onSubmit)} name="Criar Conta">
-                        <Input
-                             placeholder="Nome"
-                             type="text"
-                             isText
-                        />
-                        <Input
-                             placeholder="Sobrenome"
-                             type="text"
-                             isText
-                        />
-                        <Input
-                             placeholder="Email"
-                             type="text"
-                             isText
-                        />
-                        <Input
-                             placeholder="Senha"
-                             type="password"
-                        />
-                        <Input
-                             placeholder="Confirmar Senha"
-                             type="password"
-                        />
-                        <Select
-                            arrayText={["Vendedor", "Cliente"]}
-                            register={register}
-                            name="teste"
-                            label="Tipo de conta"
-                        />
-                        <Checkbox/>
-                        <ButtonAll background="deft" size="small">
-                            Criar conta
-                        </ButtonAll>
-                        <p className="form__optionsText">Já tem uma conta?</p>
-                        <ButtonAll background="transp" size="small">
-                            Entrar
-                        </ButtonAll>
-                        <button type="submit">teste</button>
-                    </Form>
-                </Box>
-            </RegisterStyled>
-        </TransitionPage>
-    )
-}
-export default Register
+  return (
+    <TransitionPage>
+      <RegisterStyled>
+        <h1 className="block__logo">Kenzie Livre</h1>
+        <Block>
+          <Apresentation
+            title="Não tem uma conta?"
+            description="Cadastre-se já e encontre os melhores produtos pelo menor preço."
+          />
+        </Block>
+        <Box width="small" minWidth="563px" widthMobile="100%" height="100%">
+          <Form onSubmit={handleSubmit(onSubmit)} name="Criar Conta">
+            <Input
+              placeholder="Nome"
+              type="text"
+              isText
+              register={register}
+              name="name"
+            />
+            <Input
+              placeholder="Sobrenome"
+              type="text"
+              isText
+              register={register}
+              name="lastname"
+            />
+            <Input
+              placeholder="Email"
+              type="text"
+              isText
+              register={register}
+              name="email"
+            />
+            <Input
+              placeholder="Senha"
+              type="password"
+              register={register}
+              name="password"
+            />
+            <Input
+              placeholder="Confirmar Senha"
+              type="password"
+              register={register}
+              name="confirmPassword"
+            />
+            <Select
+              arrayText={["Vendedor", "Cliente"]}
+              register={register}
+              name="teste"
+              label="Tipo de conta"
+            />
+            <Checkbox />
+            <ButtonAll background="deft" size="small">
+              Criar conta
+            </ButtonAll>
+            <p className="form__optionsText">Já tem uma conta?</p>
+            <ButtonAll background="transp" size="small">
+              Entrar
+            </ButtonAll>
+            <button type="submit">teste</button>
+          </Form>
+        </Box>
+      </RegisterStyled>
+    </TransitionPage>
+  );
+};
+export default Register;
