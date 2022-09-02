8858d4648411d15e0de8c7cca0ca3873fd1199c8
unknown <brenos93@gmail.com>
2022-09-02 15:34:26 -0300

    feat: página de criar o produto finalizada

--- src/pages/CreateProduct/style.ts
+++ src/pages/CreateProduct/style.ts
@@ -14,3 +14,97 @@ export const CreateProductStyled = styled.div`
 
   background-color: #1c1b23;
 `;
+
+export const ContainterCreateProduct = styled.div`
+  background-color: transparent;
+
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: flex-end;
+
+  @media screen and (min-width: 1250px) {
+    background-image: url("./assets/backgroundCreateProduct.svg");
+  }
+
+  @media screen and (max-width: 1250px) {
+    justify-content: center;
+  }
+`;
+
+export const FormStyled = styled.form`
+  box-sizing: border-box;
+
+  transition: 200ms;
+
+  width: 100%;
+  max-width: 320px;
+  height: 70%;
+
+  margin: 40px 0px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  button {
+    width: 100%;
+  }
+
+  .divInputs {
+    overflow-y: scroll;
+  }
+
+  .divButtons {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+  }
+
+  .form__title {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 35px;
+    line-height: 41px;
+
+    letter-spacing: 0.1em;
+
+    color: #ffffff;
+  }
+
+  .form__optionsText {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 300;
+    font-size: 15px;
+    line-height: 18px;
+
+    letter-spacing: 0.1em;
+
+    color: #ffffff;
+    padding: 23px;
+  }
+`;
+
+export const Block = styled.div`
+  display: none;
+
+  @media (min-width: 1250px) {
+    box-sizing: border-box;
+
+    transition: 200ms;
+
+    width: 100%;
+    height: 100%;
+
+    margin-top: 350px;
+
+    display: flex;
+    align-items: center;
+
+    padding: 0px 10px 105px 0px;
+  }
+`;
